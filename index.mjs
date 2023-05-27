3d24fe64c4b686535b5d3f25e597486689298ce9
Schleimfresse <87274892+Schleimfresse@users.noreply.github.com>
2023-05-27 13:19:49 +0200

    generateRandomCode v1.1

--- index.mjs
+++ index.mjs
@@ -11,6 +11,9 @@ const rl = readline.createInterface({
 	output: process.stdout,
 });
 
+/**
+ * All color codes which are needed; used to color text in the console
+ */
 const colorCodes = {
 	reset: "\x1b[0m",
 	red: "\x1b[31m",
@@ -18,22 +21,56 @@ const colorCodes = {
 	yellow: "\x1b[33m",
 	bright: "\x1b[1m",
 };
+/**
+ *
+ * @returns a random 6 digit generated code
+ * @since v.1.1.0
+ */
+const generateRandomCode = () => {
+	const characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	let code = "";
 
+	for (let i = 0; i < 6; i++) {
+		const randomIndex = Math.floor(Math.random() * characters.length);
+		code += characters[randomIndex];
+	}
+
+	return " " + code;
+};
+
+/**
+ * main function
+ * @param {*} playlistUrl URL of the playlist which gets downloaded
+ * @param {*} ext extention for the files
+ * @async
+ * @since v1.0.0
+ */
 async function downloadPlaylist(playlistUrl, ext) {
 	try {
 		const playlistId = await ytpl.getPlaylistID(playlistUrl);
 		const playlistInfo = await ytpl(playlistId);
 
 		const downloadsPath = path.join(os.homedir(), "Downloads");
-		const playlistTitle = playlistInfo.title.replace(/[^\w\s]/gi, "");
-		const playlistFolderPath = path.join(downloadsPath, playlistTitle);
+		let playlistTitle = playlistInfo.title.replace(/[^\w\s]/gi, "");
+		let playlistFolderPath = path.join(downloadsPath, playlistTitle);
+		console.log(playlistTitle);
+		let folderExists = fs.existsSync(playlistFolderPath);
+		while (folderExists) {
+			console.warn(
+				`${colorCodes.yellow}Folder at ${playlistFolderPath} already exists! Generating code for folder name...${colorCodes.reset}`
+			);
+			playlistTitle = playlistTitle + generateRandomCode();
+			playlistFolderPath = path.join(downloadsPath, playlistTitle);
+			console.log(playlistTitle, playlistFolderPath);
+			folderExists = fs.existsSync(playlistFolderPath);
+		}
 		fs.mkdirSync(playlistFolderPath);
 
 		const progressBar = new SingleBar(
 			{
-				format: `${colorCodes.bright + colorCodes.green}Downloading: ${
-					colorCodes.reset + "{bar}"
-				} | {percentage}% | {value}/{total} bytes`,
+				format: `${colorCodes.bright + colorCodes.green}Downloading: ${colorCodes.reset + "{title}"} | ${
+					"{tracknumber}" + "/" + "{trackstotal}"
+				} | {bar} | {percentage}% | {value}/{total} bytes`,
 				barCompleteChar: "\u2588",
 				barIncompleteChar: "\u2591",
 				hideCursor: true,
@@ -41,11 +78,12 @@ async function downloadPlaylist(playlistUrl, ext) {
 			Presets.shades_classic
 		);
 
-		progressBar.start(100, 0);
+		progressBar.start(100, 0, { trackstotal: playlistInfo.items.length });
 
 		for (const videoInfo of playlistInfo.items) {
 			const videoTitle = videoInfo.title.replace(/[^\w\s]/gi, "");
 			const videoUrl = videoInfo.shortUrl;
+			progressBar.update(0, { title: videoTitle, tracknumber: playlistInfo.items.indexOf(videoInfo) + 1 });
 			let videoStream;
 			switch (ext) {
 				case "mp4":
@@ -58,6 +96,8 @@ async function downloadPlaylist(playlistUrl, ext) {
 					throw new Error("No format was specified.");
 			}
 			const fileStream = fs.createWriteStream(path.join(playlistFolderPath, `${videoTitle}.${ext}`));
+
+			let receivedBytes = 0;
 			let totalBytes = 0;
 
 			videoStream.on("response", (response) => {
@@ -66,7 +106,8 @@ async function downloadPlaylist(playlistUrl, ext) {
 			});
 
 			videoStream.on("data", (chunk) => {
-				progressBar.increment(chunk.length);
+				receivedBytes += chunk.length;
+				progressBar.update(receivedBytes);
 			});
 
 			videoStream.pipe(fileStream);
@@ -76,17 +117,23 @@ async function downloadPlaylist(playlistUrl, ext) {
 				videoStream.on("error", reject);
 			});
 
-			progressBar.update(0);
-			console.log(`${colorCodes.bright + colorCodes.green}Downloaded: ${colorCodes.reset + videoTitle}`);
+			console.log(`\n${colorCodes.bright + colorCodes.green}Downloaded: ${colorCodes.reset + videoTitle}`);
 		}
 
 		progressBar.stop();
-		console.log(`${colorCodes.green}Playlist download completed.`);
+		console.log(`${colorCodes.green}Playlist download completed.${colorCodes.reset}`);
 	} catch (error) {
-		console.error(`${colorCodes.red}An error occurred:`, error);
+		console.error(`${colorCodes.red}An error occurred:${colorCodes.reset}`, error);
+	} finally {
+		rl.close();
+		process.exit(1);
 	}
 }
 
+/**
+ * User input
+ * @since v.1.0.0
+ */
 rl.question(
 	`${colorCodes.bright}Paste your YouTube playlist link here: ${colorCodes.reset}`,
 	async (YTURL) => {
@@ -111,6 +158,11 @@ rl.question(
 	}
 );
 
+/**
+ * When the user inputs a invalid media type this function re-asks for input
+ * @param {*} YTURL URL of the playlist which gets downloaded
+ * @since v.1.0.0
+ */
 const retrymediatype = (YTURL) => {
 	rl.question(
 		`${colorCodes.bright}Should the files have video and audio or only audio? (audio/video): ${colorCodes.reset}`,
@@ -128,6 +180,10 @@ const retrymediatype = (YTURL) => {
 	);
 };
 
+/**
+ * When the user inputs a invalid link this function re-asks for input
+ * @since v.1.0.0
+ */
 const retryurl = () => {
 	rl.question(
 		`${colorCodes.bright}Paste your YouTube playlist link here: ${colorCodes.reset}`,
